c448c26f92fcf89ff9fbd4c94fa3ab8c610bb352
JG <diasusb@hotmail.com>
2022-05-01 01:59:38 -0300

    Add Modo Escuro&Claro, Corrigindo erro sobre o video do youtube

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 function carregar() { 
+  let getById = (element) => window.document.getElementById(element);
   let areaLinkHTML = window.document.getElementById("areaLinkHTML"); 
   let areaLinkCSS = window.document.getElementById("areaLinkCSS"); 
   let areaLinkCursos = window.document.getElementById("areaLinkCursos"); 
+  let btnModoCE = window.document.getElementById("btnModoCE");
   let url_atual = window.location.href;
+
+  function MudarEvento(Evento01, Evento02) {
+    btnModoCE.removeEventListener("click", Evento01), btnModoCE.addEventListener("click", Evento02);
+  }
+
+  function ModoClaro() {
+    btnModoCE.setAttribute("class", "btnModoEscuro");
+    btnModoCE.innerHTML = "Modo Escuro";
+    getById("bgEscuro").removeAttribute("class", "bgEstiloEscuro");
+    getById("titulo").removeAttribute("class", "h1EstiloEscuro");
+
+    getById("freepikEm").removeAttribute("style", "");
+    getById("freepikStrong").removeAttribute("style", "");
+    getById("main01").removeAttribute("style", "");
+    getById("main02").removeAttribute("style", "");
+    getById("main03").removeAttribute("style", "");
+    MudarEvento(ModoClaro, ModoEscuro);
+  }
+
+  function ModoEscuro() {
+    btnModoCE.setAttribute("class", "btnModoClaro");
+    btnModoCE.innerHTML = "Modo Claro";
+    getById("bgEscuro").setAttribute("class", "bgEstiloEscuro");
+    getById("titulo").setAttribute("class", "h1EstiloEscuro");
+
+    getById("freepikEm").setAttribute("style", "color: white;");
+    getById("freepikStrong").setAttribute("style", "color: rgba(253, 36, 2, 0.9);");
+    getById("main01").setAttribute("style", " background-color: rgb(48, 105, 122);");
+    getById("main02").setAttribute("style", " background-color: rgb(48, 105, 122);");
+    getById("main03").setAttribute("style", " background-color: rgb(48, 105, 122);");
+    MudarEvento(ModoEscuro, ModoClaro);
+  }
+  btnModoCE.addEventListener('click', ModoEscuro);
 
   function CriarLink(link, linkTitulo, getById){
     let inserirConteudo = `
