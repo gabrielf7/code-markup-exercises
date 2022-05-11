508b82242d7bcffb371de9dcec52404f7b5e3e22
JG <diasusb@hotmail.com>
2022-05-11 15:55:47 -0300

    Corrigir link do curso

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ function carregar() {
   CriarLink("css/reflexo-espelho/reflexo-espelho.html", "Reflexo de Espelho", areaLinkCSS);
   CriarLink("css/template-colunas/teste-template.html", "Template de Colunas", areaLinkCSS);
   // Cursos
-  CriarLink("_cursos/HTML5CSS3-na-pratica-02-Curso-HTML5-para-quem-n%C3%A3o-sabe-nada-de-HTML5/www.gabrielf7.tk/index.html", "HTML5 - Paulo Andrade | Udemy", areaLinkCursos);
+  CriarLink("_cursos/HTML5CSS3-na-pratica-02-Curso-HTML5-para-quem-não-sabe-nada-de-HTML5/www.gabrielf7.tk/index.html", "HTML5 - Paulo Andrade | Udemy", areaLinkCursos);
 }
 window.addEventListener('load', carregar);
